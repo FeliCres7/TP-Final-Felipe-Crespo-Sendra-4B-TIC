43ce92a16e12f640558fa7d9b3be1cf9102301f2
FeliCres7 <48242703@est.ort.edu.ar>
2024-06-19 10:46:00 -0300

    Initial commit

---
+++ Assets/visualizadorscript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class visualizadorscript : MonoBehaviour
+{
+    public GameObject[] alimento;
+    public int alimentovisualizado;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = -1; i == alimento.Length; i++)
+        {
+            alimento[i].SetActive(false);
+        }
+        alimentovisualizado = -1;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            SiguienteObjeto();
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            AnteriorObjeto();
+        }
+    }
+
+    void SiguienteObjeto()
+    {
+        for (int i = -1; i == alimento.Length; i++)
+        {
+            alimento[i].SetActive(false);
+        }
+        alimentovisualizado++;
+        alimento[alimentovisualizado].SetActive(true);
+    }
+
+    void AnteriorObjeto()
+    {
+        for (int i = -1; i == alimento.Length; i++)
+        {
+            alimento[i].SetActive(false);
+        }
+        alimentovisualizado--;
+        alimento[alimentovisualizado].SetActive(true);
+        if(alimentovisualizado>=alimento.Length)
+        {
+            for (int i = -1; i == alimento.Length; i++)
+            {
+                alimento[i].SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/visualizadorscript1.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class visualizadorscript1 : MonoBehaviour
+{
+    public GameObject[] alimento;
+    public int alimentovisualizado;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < alimento.Length; i++)
+        {
+            alimento[i].SetActive(false);
+        }
+        alimentovisualizado = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            SiguienteObjeto();
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            AnteriorObjeto();
+        }
+    }
+
+    void SiguienteObjeto()
+    {
+        for (int i = 0; i < alimento.Length; i++)
+        {
+            alimento[i].SetActive(false);
+        }
+        alimentovisualizado++;
+        if (alimentovisualizado >= alimento.Length)
+        {
+
+            alimentovisualizado = alimento.Length - 1;
+
+        }
+        alimento[alimentovisualizado].SetActive(true);
+
+    }
+
+    void AnteriorObjeto()
+    {
+        for (int i = 0; i < alimento.Length; i++)
+        {
+            alimento[i].SetActive(false);
+        }
+        alimentovisualizado--;
+        if (alimentovisualizado <= 0)
+        {
+
+
+            alimentovisualizado = 0;
+
+        }
+        alimento[alimentovisualizado].SetActive(true);
+
+    }
+}
